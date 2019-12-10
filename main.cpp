507a1109bf0170f2af1e6bb368fa4d79778fde5c
tdoumenc <tdoumenc@btslivh.eu>
2019-12-10 15:22:13 +0100

    Le chat argenté se nourrit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: snir2g2
+ *
+ * Created on 10 décembre 2019, 15:12
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout << "La semaine est longue." << endl;
+    //le chat agenté se nourrit
+    //quand le bleu et le jaune se rencontret à l'ouest
+    //un voyage en Chine paraît sympa si vous avancez prudemment
+    return 0;
+}
+
