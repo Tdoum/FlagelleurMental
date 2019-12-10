6439dd71e6b75813a30daecacb25ad874a41dc1d
tdoumenc <tdoumenc@btslivh.eu>
2019-12-10 15:42:34 +0100

    Le chat argenté se nourrit Un voyage en chine paraît sympa si vous avancez prudemment

--- main.cpp
+++ main.cpp
@@ -23,7 +23,6 @@ int main(int argc, char** argv) {
 
     cout << "La semaine est longue." << endl;
     cout << "quand le bleu et le jaune se rencontret à l'ouest" << endl;
-    //un voyage en Chine paraît sympa si vous avancez prudemment
     return 0;
 }
 
